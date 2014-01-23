dbd886099235bd1211c9e94a4f5c9c873280d178
Tommy Yu <y@metatoaster.com>
2014-01-23 22:42:39 +1300

    Implement basic (very hilariously naive) events
    - This naive way only does things one user at a time.  We will fix this
      later.

--- src/idleiss/core.py
+++ src/idleiss/core.py
@@ -1,7 +1,11 @@
+from math import log
+
+
 class GameEngine(object):
 
     def __init__(self):
         self.users = {}
+        self.last_event_timestamp = 0
 
     def user_logged_in(self, user_id, timestamp):
         if user_id not in self.users:
@@ -22,3 +26,27 @@ class GameEngine(object):
             raise ValueError
 
         return timestamp - self.users[user_id]['last_idle']
+
+    def get_events(self, timestamp):
+        if timestamp < self.last_event_timestamp:
+            # can't go back in time for now.
+            return
+
+        self.last_event_timestamp = timestamp
+
+        result = {}
+
+        for user_id in self.users:
+            user = self.users[user_id]
+            idle_time = self.get_user_idleness(user_id, timestamp)
+            # probably have to split this to a dedicated method later.
+            user_level = int(log(idle_time, 2)) + 1
+
+            if user_level > user.get('level', 0):
+                # should probably create an event object later?
+                user['level'] = user_level
+                result[user_id] = {
+                    'new_level': user_level
+                }
+
+        return result
--- tests/test_core.py
+++ tests/test_core.py
@@ -32,3 +32,50 @@ class CoreTestCase(TestCase):
         engine.user_room_message('an_user', 'some_message', timestamp=1006)
         idleness = engine.get_user_idleness('an_user', timestamp=1010)
         self.assertEqual(idleness, 4)
+
+    def test_events_basic(self):
+        engine = core.GameEngine()
+        engine.user_logged_in('an_user', timestamp=1000)
+        engine.user_room_message('an_user', 'some_message', timestamp=1006)
+        events = engine.get_events(timestamp=1007)
+        self.assertEqual(events, {
+            'an_user': {
+                'new_level': 1,
+            }
+        })
+
+        # can't trigger the same events again I guess?
+        events = engine.get_events(timestamp=1007)
+        self.assertEqual(events, {})
+
+        events = engine.get_events(timestamp=1008)
+        self.assertEqual(events, {
+            'an_user': {
+                'new_level': 2,
+            }
+        })
+
+    def test_events_skip_time(self):
+        engine = core.GameEngine()
+        engine.user_logged_in('an_user', timestamp=1000)
+        engine.user_room_message('an_user', 'some_message', timestamp=2000)
+        events = engine.get_events(timestamp=2100)
+        self.assertEqual(events, {
+            'an_user': {
+                'new_level': 7,
+            }
+        })
+
+    def test_events_user_spoke(self):
+        engine = core.GameEngine()
+        engine.user_logged_in('an_user', timestamp=1000)
+        events = engine.get_events(timestamp=1050)
+        self.assertEqual(events, {
+            'an_user': {
+                'new_level': 6,
+            }
+        })
+        engine.user_room_message('an_user', 'some_message', timestamp=1051)
+        events = engine.get_events(timestamp=1053)
+        # would not trigger the lower level events again
+        self.assertEqual(events, {})
