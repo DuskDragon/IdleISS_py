83ef8022613e129d1a298326e109b2c59264009f
Tommy Yu <y@metatoaster.com>
2014-01-23 22:32:39 +1300

    Well, they really are just user_ids
    - instead of the actual users.

--- src/idleiss/core.py
+++ src/idleiss/core.py
@@ -3,22 +3,22 @@ class GameEngine(object):
     def __init__(self):
         self.users = {}
 
-    def user_logged_in(self, user, timestamp):
-        if user not in self.users:
-            self.users[user] = {}
-            self.users[user]['last_idle'] = timestamp
+    def user_logged_in(self, user_id, timestamp):
+        if user_id not in self.users:
+            self.users[user_id] = {}
+            self.users[user_id]['last_idle'] = timestamp
 
-    def user_room_message(self, user, message, timestamp):
-        if user not in self.users:
+    def user_room_message(self, user_id, message, timestamp):
+        if user_id not in self.users:
             return
 
-        self.users[user]['last_idle'] = timestamp
+        self.users[user_id]['last_idle'] = timestamp
 
-    def user_logged_out(self, user, timestamp):
+    def user_logged_out(self, user_id, timestamp):
         pass
 
-    def get_user_idleness(self, user, timestamp):
-        if user not in self.users:
+    def get_user_idleness(self, user_id, timestamp):
+        if user_id not in self.users:
             raise ValueError
 
-        return timestamp - self.users[user]['last_idle']
+        return timestamp - self.users[user_id]['last_idle']
