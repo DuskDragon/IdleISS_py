0c120fa152be66b5ee20b9021e29660c2528d0eb
Tommy Yu <y@metatoaster.com>
2014-01-23 21:59:19 +1300

    Base core implementation

--- src/idleiss/core.py
+++ src/idleiss/core.py
@@ -1,13 +1,24 @@
 class GameEngine(object):
 
+    def __init__(self):
+        self.users = {}
+
     def user_logged_in(self, user, timestamp=None):
-        pass
+        if user not in self.users:
+            self.users[user] = {}
+            self.users[user]['last_idle'] = timestamp
 
     def user_room_message(self, user, message, timestamp=None):
-        pass
+        if user not in self.users:
+            return
+
+        self.users[user]['last_idle'] = timestamp
 
     def user_logged_out(self, user, timestamp=None):
         pass
 
     def get_user_idleness(self, user, timestamp=None):
-        pass
+        if user not in self.users:
+            raise ValueError
+
+        return timestamp - self.users[user]['last_idle']
