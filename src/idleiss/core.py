2c3e957cc71d5d1a3d51fbfbdd399c8a137b1ce9
Tommy Yu <y@metatoaster.com>
2014-01-23 22:06:47 +1300

    Let's take out the magic that won't happen in future.

--- src/idleiss/core.py
+++ src/idleiss/core.py
@@ -3,21 +3,21 @@ class GameEngine(object):
     def __init__(self):
         self.users = {}
 
-    def user_logged_in(self, user, timestamp=None):
+    def user_logged_in(self, user, timestamp):
         if user not in self.users:
             self.users[user] = {}
             self.users[user]['last_idle'] = timestamp
 
-    def user_room_message(self, user, message, timestamp=None):
+    def user_room_message(self, user, message, timestamp):
         if user not in self.users:
             return
 
         self.users[user]['last_idle'] = timestamp
 
-    def user_logged_out(self, user, timestamp=None):
+    def user_logged_out(self, user, timestamp):
         pass
 
-    def get_user_idleness(self, user, timestamp=None):
+    def get_user_idleness(self, user, timestamp):
         if user not in self.users:
             raise ValueError
 
