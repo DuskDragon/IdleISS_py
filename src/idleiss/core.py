434b91c97744b47b05631fb64980c94d99774cca
Tommy Yu <y@metatoaster.com>
2014-01-23 21:38:14 +1300

    More simple core designs and tests.

--- src/idleiss/core.py
+++ src/idleiss/core.py
@@ -1,2 +1,13 @@
 class GameEngine(object):
-    pass
+
+    def user_logged_in(self, user, timestamp=None):
+        pass
+
+    def user_room_message(self, user, message, timestamp=None):
+        pass
+
+    def user_logged_out(self, user, timestamp=None):
+        pass
+
+    def get_user_idleness(self, user, timestamp=None):
+        pass
--- tests/test_core.py
+++ tests/test_core.py
@@ -14,3 +14,19 @@ class CoreTestCase(TestCase):
     def test_base_game(self):
         engine = core.GameEngine()
         self.assertTrue(engine)
+
+    def test_no_such_user(self):
+        engine = core.GameEngine()
+
+    def test_base_login(self):
+        engine = core.GameEngine()
+        engine.user_logged_in('an_user', timestamp=1000)
+        idleness = engine.get_user_idleness('an_user', timestamp=1010)
+        self.assertEqual(idleness, 10)
+
+    def test_user_interaction(self):
+        engine = core.GameEngine()
+        engine.user_logged_in('an_user', timestamp=1000)
+        engine.user_room_message('an_user', 'some_message', timestamp=1006)
+        idleness = engine.get_user_idleness('an_user', timestamp=1010)
+        self.assertEqual(idleness, 4)
