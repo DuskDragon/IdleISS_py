fc78427c34b806bcbdfc4ff90f44291b46ed6058
DuskDragon <duskdragon@gmail.com>
2014-01-26 06:18:19 -0500

    Introducing the ShipLibrary - loads all the ship data from config json files

--- src/idleiss/fleet.py
+++ src/idleiss/fleet.py
@@ -1,9 +1,27 @@
+from os.path import join, dirname, abspath
+import json
 
+class ShipLibrary(object):
+    def __init__(self, library_filename=None):
+        self.raw_data = None
+        if library_filename:
+            self.load(library_filename)
 
-class Ship(object):
-    def __init__(self):
-        pass
+    def load(self, filename):
+        fd = open(filename)
+        self.raw_data = json.load(fd)
+        fd.close()
+        if "sizes" not in self.raw_data:
+            raise ValueError("ship sizes not found")
+        if "ships" not in self.raw_data:
+            raise ValueError("ship data not found")
+        self.size_data = self.raw_data['sizes']
+        self.ship_data = self.raw_data['ships']
 
 class FleetManager(object):
     def __init__(self):
+        pass
+
+class Battle(object):
+    def __init__(self, attacker, defender, rounds):
         pass
--- tests/test_fleet.py
+++ tests/test_fleet.py
@@ -1,6 +1,20 @@
+from os.path import join, dirname, abspath
 from unittest import TestCase
 
 from idleiss import fleet
 
+class ShipTestCase(TestCase):
+    pass
+
+class FleetLibraryTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_load_library(self):
+        test_file_name = 'testlibrary1.json'
+        target_path = join(dirname(__file__), test_file_name)
+        self.library = fleet.ShipLibrary(target_path)
+
 class FleetTestCase(TestCase):
-     pass
+    pass
