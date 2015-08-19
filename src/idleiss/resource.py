a9c230ac20be6cd9ed713683edfe55f7091bfd76
DuskDragon <duskdragon@gmail.com>
2015-08-19 18:37:09 -0400

    Adding initial resource manager code and tests

--- src/idleiss/resource.py
+++ src/idleiss/resource.py
@@ -1,4 +1,14 @@
+class Location_Already_Exists(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
 
+class Location_Does_Not_Exist(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
 
 class ResourceManager(object):
     def __init__(self):
@@ -9,8 +19,46 @@ class ResourceManager(object):
         self.basic_materials_income = 0
         self.advanced_materials_income = 0
         self.money_income = 0
+        self.income_sources = {}
+        #income sources are a nested dict of
+        # {key:system, value:{key:location, value:[type (moon, station, belt, or other), basic_income, adv_income, money income]
+        #example:
+        #{"START SYSTEM": {
+        #   "ISS": ["station", 2, 1, 1]
+        #   }
+        #}
 
     def pay_resources(self, seconds):
         self.basic_materials += self.basic_materials_income*seconds
         self.advanced_materials += self.advanced_materials_income*seconds
         self.money += self.money_income*seconds
+
+    def add_income_source(self, system, location, source_type, basic_income,
+                          adv_income, money_income):
+        self.basic_materials_income += basic_income
+        self.advanced_materials_income += adv_income
+        self.money_income += money_income
+        if system in self.income_sources:
+            if location in self.income_sources[system]:
+                raise Location_Already_Exists(str(location) + "@" + str(system) + " already exists.")
+            else:
+                self.income_sources[system].update({location: [source_type, basic_income, adv_income, money_income]})
+        else:
+            self.income_sources.update({system: {location: [source_type, basic_income, adv_income, money_income]}})
+
+    def remove_income_source(self, system, location):
+        if system in self.income_sources:
+            if location in self.income_sources[system]:
+                type, b_i, a_i, m_i = self.income_sources[system][location]
+                self.basic_materials_income -= b_i
+                self.advanced_materials_income -= a_i
+                self.money_income -= m_i
+                if self.basic_materials_income < 0 or self.advanced_materials_income < 0 or self.money_income < 0:
+                    raise ValueError("Income is negative after removing income source "+str(location)+"@"+str(system)+": " + str(self.basic_materials_income) + " " + str(self.advanced_materials_income) + " " + str(self.money_income))
+                self.income_sources[system].pop(location)
+                if len(self.income_sources[system]) == 0:
+                    self.income_sources.pop(system)
+            else:
+                raise Location_Does_Not_Exist(str(location)+"@"+str(system)+" does not exist.")
+        else:
+            raise Location_Does_Not_Exist(str(location)+"@"+str(system)+" does not exist.")
--- tests/test_resource.py
+++ tests/test_resource.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 from idleiss import resource
 
 class ResourceTestCase(TestCase):
-     def test_resource_payment(self):
+    def test_resource_payment(self):
         resources = resource.ResourceManager()
         resources.basic_materials_income = 1
         resources.advanced_materials_income = 1
@@ -12,3 +12,52 @@ class ResourceTestCase(TestCase):
         self.assertEqual(resources.basic_materials, 10)
         self.assertEqual(resources.advanced_materials, 10)
         self.assertEqual(resources.money, 10)
+
+    def test_resource_source_add(self):
+        resources = resource.ResourceManager()
+        resources.add_income_source("Sol", "Earth", "planet", 50, 50, 100)
+        self.assertEqual(resources.basic_materials_income, 50)
+        self.assertEqual(resources.advanced_materials_income, 50)
+        self.assertEqual(resources.money_income, 100)
+        self.assertEqual(resources.income_sources, {"Sol": {"Earth": ["planet", 50, 50 , 100]}})
+        resources.add_income_source("Sol", "Moon", "moon", 5, 5, 10)
+        self.assertEqual(resources.basic_materials_income, 55)
+        self.assertEqual(resources.advanced_materials_income, 55)
+        self.assertEqual(resources.money_income, 110)
+        self.assertEqual(resources.income_sources, {"Sol": {"Earth": ["planet", 50, 50 , 100], "Moon": ["moon", 5, 5, 10]}})
+
+    def test_resource_source_removal(self):
+        resources = resource.ResourceManager()
+        resources.add_income_source("Sol", "Earth", "planet", 50, 50, 100)
+        resources.remove_income_source("Sol", "Earth")
+        self.assertEqual(resources.basic_materials_income, 0)
+        self.assertEqual(resources.advanced_materials_income, 0)
+        self.assertEqual(resources.money_income, 0)
+        self.assertEqual(resources.income_sources, {})
+
+    def test_invalid_source_add(self):
+        resources = resource.ResourceManager()
+        resources.add_income_source("Sol", "Earth", "planet", 50, 50, 100)
+        with self.assertRaises(resource.Location_Already_Exists) as context:
+            resources.add_income_source("Sol", "Earth", "planet", 100, 100, 100)
+        self.assertEqual(str(context.exception), "'Earth@Sol already exists.'")
+
+    def test_invalid_source_removal(self):
+        resources = resource.ResourceManager()
+        with self.assertRaises(resource.Location_Does_Not_Exist) as context:
+            resources.remove_income_source("Sol", "Earth")
+        self.assertEqual(str(context.exception), "'Earth@Sol does not exist.'")
+        resources.add_income_source("Sol", "Moon", "moon", 5, 5, 10)
+        with self.assertRaises(resource.Location_Does_Not_Exist) as context:
+            resources.remove_income_source("Sol", "Earth")
+        self.assertEqual(str(context.exception), "'Earth@Sol does not exist.'")
+
+    def test_negative_income_after_removal(self):
+        resources = resource.ResourceManager()
+        resources.add_income_source("Sol", "Earth", "planet", 50, 50, 100)
+        resources.basic_materials_income = 0
+        resources.advanced_materials_income = 0
+        resources.money_income = 0
+        with self.assertRaises(ValueError) as context:
+            resources.remove_income_source("Sol", "Earth")
+        self.assertEqual(str(context.exception), "Income is negative after removing income source Earth@Sol: -50 -50 -100")
